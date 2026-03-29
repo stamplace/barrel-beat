c33e1a1f000b8a380aca2ebce53cb8ad24a501db
stamplace <922levy@gmail.com>
2026-03-29 10:53:52 +0300

    feat(game): add coherent retro placeholder theme

--- src/game/entities/BarrelFactory.ts
+++ src/game/entities/BarrelFactory.ts
@@ -11,10 +11,30 @@ export function createBarrel(
   x: number,
   y: number,
 ): Phaser.GameObjects.Container {
-  const outer = scene.add.circle(0, 0, 13, 0xd97706);
-  const bandL = scene.add.rectangle(-6, 0, 3, 22, 0x78350f);
-  const bandR = scene.add.rectangle(6, 0, 3, 22, 0x78350f);
-  const middle = scene.add.rectangle(0, 0, 4, 22, 0x92400e);
+  const shadow = scene.add.ellipse(0, 12, 20, 6, 0x000000, 0.16);
 
-  return scene.add.container(x, y, [outer, bandL, bandR, middle]);
+  const outer = scene.add
+    .circle(0, 0, 13, 0xd9811d)
+    .setStrokeStyle(2, 0x6b3415, 0.95);
+
+  const ringL = scene.add.rectangle(-6, 0, 2.5, 22, 0x7a4a20).setStrokeStyle(1, 0x4a2c13, 0.9);
+  const ringR = scene.add.rectangle(6, 0, 2.5, 22, 0x7a4a20).setStrokeStyle(1, 0x4a2c13, 0.9);
+  const center = scene.add.rectangle(0, 0, 3, 22, 0x9a5b24).setStrokeStyle(1, 0x5b3517, 0.8);
+
+  const stripe1 = scene.add.rectangle(-2, -7, 16, 2.5, 0xb66419).setAngle(18);
+  const stripe2 = scene.add.rectangle(2, 0, 16, 2.5, 0xb66419).setAngle(18);
+  const stripe3 = scene.add.rectangle(-2, 7, 16, 2.5, 0xb66419).setAngle(18);
+
+  const boltL = scene.add.circle(-6, -8, 1.2, 0xe9c28a);
+  const boltR = scene.add.circle(6, 8, 1.2, 0xe9c28a);
+  const shine = scene.add.ellipse(-4, -5, 7, 4, 0xffd08a, 0.22).setAngle(-28);
+
+  return scene.add.container(x, y, [
+    shadow,
+    outer,
+    stripe1, stripe2, stripe3,
+    ringL, ringR, center,
+    boltL, boltR,
+    shine,
+  ]);
 }
--- src/game/entities/HeroFactory.ts
+++ src/game/entities/HeroFactory.ts
@@ -1,17 +1,75 @@
+function outlinedRect(
+  scene: Phaser.Scene,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  fillColor: number,
+  strokeColor = 0x2b1607,
+  strokeWidth = 2,
+): Phaser.GameObjects.Rectangle {
+  return scene.add
+    .rectangle(x, y, width, height, fillColor)
+    .setStrokeStyle(strokeWidth, strokeColor, 0.95);
+}
+
+function outlinedCircle(
+  scene: Phaser.Scene,
+  x: number,
+  y: number,
+  radius: number,
+  fillColor: number,
+  strokeColor = 0x2b1607,
+  strokeWidth = 2,
+): Phaser.GameObjects.Arc {
+  return scene.add
+    .circle(x, y, radius, fillColor)
+    .setStrokeStyle(strokeWidth, strokeColor, 0.95);
+}
+
 export function createHero(
   scene: Phaser.Scene,
   x: number,
   y: number,
 ): Phaser.GameObjects.Container {
-  const legL = scene.add.rectangle(-5, 19, 4, 10, 0x60a5fa);
-  const legR = scene.add.rectangle(5, 19, 4, 10, 0x60a5fa);
-  const armL = scene.add.rectangle(-12, 5, 4, 10, 0xffdfb5);
-  const armR = scene.add.rectangle(12, 5, 4, 10, 0xffdfb5);
-  const body = scene.add.rectangle(0, 6, 18, 18, 0xff7b00);
-  const head = scene.add.circle(0, -10, 8, 0xffdfb5);
-  const hat = scene.add.rectangle(0, -17, 18, 5, 0xd62828);
+  const shadow = scene.add.ellipse(0, 24, 26, 8, 0x000000, 0.18);
 
-  return scene.add.container(x, y, [legL, legR, armL, armR, body, head, hat]);
+  const bootL = outlinedRect(scene, -5, 23, 7, 5, 0x1e293b);
+  const bootR = outlinedRect(scene, 5, 23, 7, 5, 0x1e293b);
+
+  const legL = outlinedRect(scene, -5, 16, 6, 11, 0x5fa8ff);
+  const legR = outlinedRect(scene, 5, 16, 6, 11, 0x5fa8ff);
+
+  const torso = outlinedRect(scene, 0, 4, 20, 20, 0xf97316);
+  const belt = outlinedRect(scene, 0, 10, 20, 4, 0x6b3f1d, 0x1f1207, 1);
+  const buttonL = outlinedCircle(scene, -4, 4, 1.8, 0xffe082, 0x6b3f1d, 1);
+  const buttonR = outlinedCircle(scene, 4, 4, 1.8, 0xffe082, 0x6b3f1d, 1);
+
+  const armL = outlinedRect(scene, -14, 4, 5, 12, 0xffd9b5);
+  const armR = outlinedRect(scene, 14, 4, 5, 12, 0xffd9b5);
+  const gloveL = outlinedRect(scene, -14, 11, 6, 5, 0x24180f, 0x24180f, 1);
+  const gloveR = outlinedRect(scene, 14, 11, 6, 5, 0x24180f, 0x24180f, 1);
+
+  const head = outlinedCircle(scene, 0, -10, 8, 0xffd9b5);
+  const hair = outlinedRect(scene, 0, -14, 16, 4, 0x6b3415);
+  const nose = outlinedRect(scene, 0, -9, 3, 4, 0xe7b98d, 0xb17a52, 1);
+  const eyeL = outlinedCircle(scene, -3, -11, 1.4, 0xffffff, 0x3a2412, 1);
+  const eyeR = outlinedCircle(scene, 3, -11, 1.4, 0xffffff, 0x3a2412, 1);
+  const pupilL = outlinedCircle(scene, -3, -11, 0.6, 0x24180f, 0x24180f, 1);
+  const pupilR = outlinedCircle(scene, 3, -11, 0.6, 0x24180f, 0x24180f, 1);
+
+  const capTop = outlinedRect(scene, 0, -19, 18, 5, 0xd62828);
+  const capBrim = outlinedRect(scene, 4, -16, 10, 2, 0x8f1616, 0x5b0d0d, 1);
+
+  return scene.add.container(x, y, [
+    shadow,
+    bootL, bootR,
+    legL, legR,
+    torso, belt, buttonL, buttonR,
+    armL, armR, gloveL, gloveR,
+    head, hair, nose, eyeL, eyeR, pupilL, pupilR,
+    capTop, capBrim,
+  ]);
 }
 
 export function createBoss(
@@ -19,14 +77,44 @@ export function createBoss(
   x: number,
   y: number,
 ): Phaser.GameObjects.Container {
-  const body = scene.add.ellipse(0, 0, 46, 34, 0x8b4513);
-  const head = scene.add.circle(-4, -22, 12, 0xb45309);
-  const eyeL = scene.add.circle(-8, -24, 2, 0xffffff);
-  const eyeR = scene.add.circle(0, -24, 2, 0xffffff);
-  const arm = scene.add.rectangle(18, -2, 20, 8, 0x8b4513);
-  const barrel = scene.add.circle(28, 2, 8, 0xd97706);
+  const shadow = scene.add.ellipse(0, 18, 54, 10, 0x000000, 0.2);
 
-  return scene.add.container(x, y, [body, head, eyeL, eyeR, arm, barrel]);
+  const legL = outlinedRect(scene, -13, 12, 10, 10, 0x7a3b14);
+  const legR = outlinedRect(scene, 5, 12, 10, 10, 0x7a3b14);
+
+  const torso = scene.add
+    .ellipse(0, 0, 52, 36, 0x9a4f1d)
+    .setStrokeStyle(2, 0x4a240c, 0.95);
+
+  const chest = scene.add
+    .ellipse(0, 4, 22, 16, 0xc48a56)
+    .setStrokeStyle(1, 0x6b3415, 0.8);
+
+  const armL = outlinedRect(scene, -24, 3, 14, 8, 0x8c4517);
+  const armR = outlinedRect(scene, 24, 3, 18, 8, 0x8c4517);
+  const fistL = outlinedCircle(scene, -31, 4, 5, 0x9a4f1d);
+  const fistR = outlinedCircle(scene, 34, 6, 6, 0x9a4f1d);
+
+  const head = outlinedCircle(scene, -4, -19, 13, 0x8f4517);
+  const brow = outlinedRect(scene, -4, -22, 14, 4, 0x4a240c, 0x4a240c, 1);
+  const eyeL = outlinedCircle(scene, -8, -19, 1.8, 0xffffff, 0x24180f, 1);
+  const eyeR = outlinedCircle(scene, 0, -19, 1.8, 0xffffff, 0x24180f, 1);
+  const pupilL = outlinedCircle(scene, -8, -19, 0.7, 0x24180f, 0x24180f, 1);
+  const pupilR = outlinedCircle(scene, 0, -19, 0.7, 0x24180f, 0x24180f, 1);
+
+  const mouth = outlinedRect(scene, -4, -13, 8, 2, 0x4a240c, 0x4a240c, 1);
+  const heldBarrel = outlinedCircle(scene, 28, 4, 8, 0xce7a18, 0x6b3415, 2);
+  const heldRingL = outlinedRect(scene, 24, 4, 2, 12, 0x7a4a20, 0x7a4a20, 1);
+  const heldRingR = outlinedRect(scene, 32, 4, 2, 12, 0x7a4a20, 0x7a4a20, 1);
+
+  return scene.add.container(x, y, [
+    shadow,
+    legL, legR,
+    torso, chest,
+    armL, armR, fistL, fistR,
+    head, brow, eyeL, eyeR, pupilL, pupilR, mouth,
+    heldBarrel, heldRingL, heldRingR,
+  ]);
 }
 
 export function createGoal(
@@ -34,9 +122,11 @@ export function createGoal(
   x: number,
   y: number,
 ): Phaser.GameObjects.Container {
-  const glow = scene.add.circle(0, -8, 14, 0x38bdf8, 0.35);
-  const beacon = scene.add.rectangle(0, 0, 12, 32, 0x7dd3fc);
-  const top = scene.add.circle(0, -18, 8, 0xe0f2fe);
+  const glow = scene.add.circle(0, -16, 20, 0x38bdf8, 0.18);
+  const pole = outlinedRect(scene, 0, 0, 6, 34, 0xa5c7d8, 0x2b4a56, 1);
+  const flag = outlinedRect(scene, 10, -12, 18, 10, 0x7dd3fc, 0x2b4a56, 1).setOrigin(0.5);
+  const cap = outlinedCircle(scene, 0, -18, 7, 0xe0f2fe, 0x2b4a56, 1);
+  const star = scene.add.star(0, -18, 5, 3, 6, 0xffffff, 0.9).setScale(0.55);
 
-  return scene.add.container(x, y, [glow, beacon, top]);
+  return scene.add.container(x, y, [glow, pole, flag, cap, star]);
 }
